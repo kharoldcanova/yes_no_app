001e4a645e09c722f83b78d6415e1025a233030a
kharoldcanova <kharoldsamir@gmail.com>
2023-06-02 16:32:45 -0500

    apptheme

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color.fromARGB(194, 12, 16, 213);
+
+const List<Color> _colorThemes = [
+  _customColor,
+  Colors.blue,
+  Colors.yellow,
+  Colors.black,
+  Colors.cyan,
+];
+
+class AppTheme {
+
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0}): assert(selectedColor >= 0 && selectedColor <= _colorThemes.length - 1, 
+  'Colors theme between in 0 and ${_colorThemes.length}');
+
+  ThemeData theme(){
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: _colorThemes[0],
+      brightness: Brightness.dark,
+    );
+  }
+}
