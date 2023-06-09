770736b3c722dd15506f0ba7eacf7c9e735a707b
kharoldcanova <kharoldsamir@gmail.com>
2023-06-08 21:08:57 -0500

    loading image

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -47,6 +47,15 @@ class _ImageBubble extends StatelessWidget {
           width: size.width * 0.7,
           height: 150,
           fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) return child;
+            return Container(
+              width: size.width * 0.7,
+              height: 150,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: const Text('El usuario ha enviado una imagen')
+            );
+          },
         ));
   }
 }
