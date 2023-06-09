988252f982de6b6f7bdfe630e926f7b7cb739794
kharoldcanova <kharoldsamir@gmail.com>
2023-06-09 12:16:31 -0500

    provider + message in frontend

---
+++ lib/domain/entities/message.dart
@@ -0,0 +1,13 @@
+enum FromWho { me, hers }
+
+class Message {
+  final String text;
+  final String? url;
+  final FromWho fromWho;
+
+  Message({
+    required this.text, 
+    this.url, 
+    required this.fromWho
+  });
+}
